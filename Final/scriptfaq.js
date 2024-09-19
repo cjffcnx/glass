5fc77d3dcbe71c58b95f00276e3b876eceba1e74
Srijan Gajurel_id_56 <105741237+cjffcnx@users.noreply.github.com>
2024-09-19 10:37:44 +0545

    Add files via upload

---
+++ Final/scriptfaq.js
@@ -0,0 +1,6 @@
+// Your existing JavaScript code
+
+function showAnswer(button) {
+    const answer = button.nextElementSibling;
+    answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
+}
